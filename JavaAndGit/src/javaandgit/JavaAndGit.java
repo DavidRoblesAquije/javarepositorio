05bbff399a62001e8c8b339f05c3296c35d6cfb1
david <contact@faztweb.com>
2020-05-13 17:31:44 -0500

    Segunda version de Java en Git

--- JavaAndGit/src/javaandgit/JavaAndGit.java
+++ JavaAndGit/src/javaandgit/JavaAndGit.java
@@ -16,6 +16,7 @@ public class JavaAndGit {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hola mundo desde Java para Git");
     }
     
 }
