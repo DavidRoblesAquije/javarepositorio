2117a367fefa5042e096523146d0f6b2a2f070b1
DavidRoblesAquije <46868065+DavidRoblesAquije@users.noreply.github.com>
2020-05-20 14:14:07 -0500

    Update JavaAndGit.java

--- JavaAndGit/src/javaandgit/JavaAndGit.java
+++ JavaAndGit/src/javaandgit/JavaAndGit.java
@@ -16,7 +16,7 @@ public class JavaAndGit {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola mundo desde Java para Git");
+        System.out.println("Hola mundo desde Java para Git - David robles");
         System.out.println("Esto fue editado en el mismo github");
         System.out.println("Volver a repasar el uso de git editando algo nuevo en java");
         System.out.println("Esta linea esta mal, debo restaurar la version anterior");
